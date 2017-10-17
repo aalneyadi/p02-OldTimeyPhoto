0a8bc7695825dbc5593ed2c5c54ed2414f05c0d1
aalneyadi <aalneyadi@mail.csuchico.edu>
2017-10-16 19:08:55 -0700

    Old Timey

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+// Name: Ali Alneyadi
+
 #include <iostream>
 #include "bitmap.h"
 #include <string>
@@ -5,22 +7,58 @@
 
 using namespace std;
 
- int main () 
+int main () 
 
- { 
+{ 
+  Bitmap image;
+  vector <vector <Pixel> > bmp;
+  Pixel rgb;
+  string bmpFile;
 
- Bitmap image;
- vector <vector <Pixel> > bmp;
- Pixel rgb;
+  // Ask the user for the file name
+  cout<<"File name : "<<endl;
+  getline(cin,bmpFile);
+
+  image.open(bmpFile);
+
+  // Keep asking for the file name if it is not a valid bmp file!
+  while (image.isImage()==false)
+  {
+    cout<<"Please enter a 24 bit depth Windows BMP image."<<endl;
+    cout<<"Try again!!"<<endl;
+    cout<<"File name : "<<endl;
+    getline(cin,bmpFile);
+    image.open(bmpFile);
+  }
+
+  bmp = image.toPixelMatrix();
  
- image.open("machupicchu.dib.bmp");
- bmp = image.toPixelMatrix();
- 
- cout<<"machupicchu.dib.bmp has been loaded. it is "<<bmp[0].size()<<" pixels wide and "<<bmp.size()<<" pixels high."<<endl;
+  cout<<bmpFile<<" has been loaded. it is "<<bmp[0].size()<<" pixels wide and "<<bmp.size()<<" pixels high."<<endl;
 
 
- return 0; 
- } 
+  // Make the oldtimey bmp
+  for(int row=0;row<bmp.size();row++)
+  {
+    for(int col=0;col<bmp[row].size();col++)
+    {
+      // Make each pixel the average of the rgb
+      int avg;
+      avg = bmp[row][col].red;
+      avg += bmp[row][col].green;
+      avg += bmp[row][col].blue;
+      avg /= 3;
 
+      rgb.red = avg;
+      rgb.blue = avg;
+      rgb.green = avg;
 
+      bmp[row][col] = rgb;
+    }
+  }
+  // All done
+  image.fromPixelMatrix(bmp);
+  image.save("oldtimey.bmp");
+  cout<<"Saved the grayscale version of the image "<<bmpFile<<" as oldtimey.bmp!"<<endl;
 
+  return 0; 
+} 
